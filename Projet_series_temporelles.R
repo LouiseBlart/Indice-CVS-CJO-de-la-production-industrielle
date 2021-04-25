43d51b7f7933f148be10d46619ed88d23d2e2bd6
JeanneAstier <jeanne.astier@hec.edu>
2021-04-25 12:45:03 +0200

    Tests de stationnarité
    On vérifie que la série brute n'est pas stationnaire mais que la série différenciée l'est - tout marche bien, c'est fantastique.

--- Projet_series_temporelles.R
+++ Projet_series_temporelles.R
@@ -10,6 +10,7 @@ rm(list=ls())
 #install.packages("tseries")
 require(zoo)
 require(tseries)
+require(fUnitRoots)
 
 ### PARTIE 1 : LES DONNEES
 
@@ -58,8 +59,72 @@ data <- zoo(data['Indice'], order.by = Periode)
 diff_data <- diff(data, 1)
 plot(diff_data, type = "l", xlab = "Periode", ylab = "Indice différencié", col = "royalblue3")
 
-plot(cbind(data, diff_data))
+# la série différenciée a l'air stationnaire. 
 
+# pour vérifier ces impressions, on réalise des tests de racine unitaire ADF. 
+
+# test ADF sur la série data
+# régression pour savoir dans quel cas on se trouve : tendance et ou/ constante ? 
 summary(lm(data~Periode))
+# la constante et le coefficient de "Periode" sont significatifs
+# => tendance avec constante
 
-     
+# détermination du lag k avec lequel effectuer le test ADF :
+
+# fonction qui teste la blancheur d'une série (test de Ljung-Box) : 
+Qtests <- function(series, k, fitdf=0) {
+  pvals <- apply(matrix(1:k), 1, FUN=function(l) {
+    pval <- if (l<=fitdf) NA else Box.test(series, lag=l, type="Ljung-Box", fitdf=fitdf)$p.value
+    return(c("lag"=l,"pval"=pval))
+  })
+  return(t(pvals))
+}
+# fonction qui détermine l'ordre auquel un test ADF est valide 
+# i.e. effectue des tests ADF avec des lags croissants jusqu'à obtenir des résidus non autocorrélés
+adfTest_valid <- function(series,kmax,type){
+  k <- 0
+  noautocorr <- 0
+  while (noautocorr==0){
+    cat(paste0("ADF with ",k, " lags: residuals OK? "))
+    adf <- adfTest(series,lags=k,type=type)
+    pvals <- Qtests(adf@test$lm$residuals,kmax,fitdf=length(adf@test$lm$coefficients))[,2]
+    if (sum(pvals<0.05,na.rm=T) == 0) {
+      noautocorr <- 1; cat("OK \n")}
+    else cat("nope \n")
+    k <- k + 1
+  }
+  return(adf)
+}
+adf <- adfTest_valid(data, 24, 'ct')
+# il faut donc 21 lags pour obtenir un test ADF valide
+adf
+# p-value grande 
+# => série non stationnaire. 
+
+# test ADF sur la série dff_data :
+
+summary(lm(diff_data~Periode[-1]))
+# les deux p-values sont grandes => coeff non significatifs
+# => pas de tendance et pas de constante : type 'nc'
+adf <- adfTest_valid(diff_data, 24, 'nc')
+# il faut 17 lags pour obtenir un test ADF valide 
+adf
+# p-value petite 
+# => série stationnaire. 
+
+# On vérifie que le test de Perron-Philipps donne une résultat cohérent :  
+pp.test(diff_data)
+# p-value petite, donc hypothèse nulle de racine unitaire rejetée : cohérent
+
+# idem avec le test KPSS : 
+kpss.test(diff_data)
+# p-value grande, donc hypothèse nulle de stationnarité non-rejetée : cohérent
+
+# CONCLUSION : la série différenciée est bien stationnaire, youpi. 
+
+par(mfrow = c(1, 2)) # dispose les graphiques sur 1 ligne
+plot(data, type = "l", xlab = "Periode", ylab = "Indice différencié", col = "royalblue3")
+plot(diff_data, type = "l", xlab = "Periode", ylab = "Indice différencié", col = "royalblue3")
+
+
+
